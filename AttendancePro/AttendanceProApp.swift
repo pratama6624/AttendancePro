e89aef107368f8a5db635b41445480cf1c690a1b
pratama6624 <scyoung6624@gmail.com>
2024-07-22 21:47:13 +0700

    Initial Commit

---
+++ AttendancePro/AttendanceProApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceProApp.swift
+//  AttendancePro
+//
+//  Created by Pratama One on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AttendancePro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AttendancePro
+//
+//  Created by Pratama One on 22/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
