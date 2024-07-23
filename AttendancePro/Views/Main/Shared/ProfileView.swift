0b43b849ce3f88888f30af38d4558145c9c145e9
pratama6624 <scyoung6624@gmail.com>
2024-07-23 08:06:03 +0700

    - Adding Main.Shared Views
    - DashboardView
    
    - ProfileView
    
    - SettingsView

---
+++ AttendancePro/Views/Main/Shared/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  AttendancePro
+//
+//  Created by Pratama One on 23/07/24.
+//
+
+import SwiftUI
+
+// UI for user profile management
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
