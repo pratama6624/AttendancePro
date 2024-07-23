736ae13bb543b4bf953185790c84244abefdbb4d
pratama6624 <scyoung6624@gmail.com>
2024-07-23 07:56:46 +0700

    Adding Main.Manager Views
    - ManageScheduleView
    
    - ApproveLeaveView
    
    - AttendanceReportsView
    
    - ManagerNotificationsView

---
+++ AttendancePro/Views/Main/Manager/ApproveLeaveView.swift
@@ -0,0 +1,19 @@
+//
+//  ApproveLeaveView.swift
+//  AttendancePro
+//
+//  Created by Pratama One on 23/07/24.
+//
+
+import SwiftUI
+
+// UI to approve or reject leave and permit applications
+struct ApproveLeaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApproveLeaveView()
+}
---
+++ AttendancePro/Views/Main/Manager/AttendanceReportsView.swift
@@ -0,0 +1,19 @@
+//
+//  AttendanceReportsView.swift
+//  AttendancePro
+//
+//  Created by Pratama One on 23/07/24.
+//
+
+import SwiftUI
+
+// UI to view team attendance reports
+struct AttendanceReportsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttendanceReportsView()
+}
